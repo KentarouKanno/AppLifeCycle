a918f388858256d38ff2e6e5cfc6fcabbf6dab31
KentarOu <KenrarOu@KentarOu-no-MacBook-Air.local>
2013-08-18 11:16:37 +0900

    Initial Commit

---
+++ AppLifeCycle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppLifeCycle
+//
+//  Created by KentarOu on 2013/08/18.
+//  Copyright (c) 2013年 @_KentarOu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppLifeCycle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppLifeCycle
+//
+//  Created by KentarOu on 2013/08/18.
+//  Copyright (c) 2013年 @_KentarOu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
